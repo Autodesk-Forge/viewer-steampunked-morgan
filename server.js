6e77d09d17453a2d92611b52a5b626fe46c212f2
Kean Walmsley <kean.walmsley@autodesk.com>
2014-07-11 09:54:58 +0200

    Port change
    Former-commit-id: 86f6e1ea28a697779127006de3ef47043382c1f8

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get('/steamed/api/token', api.getToken);
 //  
 //
 /////////////////////////////////////////////////////////////////////////////////
-app.listen(3000);//process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log('Listening on port 3000...');
